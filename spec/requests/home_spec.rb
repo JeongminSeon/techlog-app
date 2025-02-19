f3a171c23a566f22c535c78ca65a19fb901817de
JeongminSeon <skelters@naver.com>
2025-02-20 00:03:07 +0900

    トップページの統合テストを追加

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,15 +1,10 @@
-require 'rails_helper'
+require 'rails_helper' # rails_helper で定義した設定を読み込み
 
-RSpec.describe 'Home', type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
-  describe 'トップページの検証' do
-    it 'Home#top という文字列が表示される' do
-      visit '/'
-
-      expect(page).to have_content('Home#top')
+RSpec.describe 'Home', type: :request do # テスト対象モジュールとテストの種類
+  describe 'GET /' do # テストする機能に関する説明
+    it 'HTTP ステータス 200 を返す' do # どういう結果を期待しているか
+      get '/' # 確認したい操作: / というパスに GET でリクエストを投げる
+      expect(response).to have_http_status(200) # 期待する確認結果
     end
   end
 end
